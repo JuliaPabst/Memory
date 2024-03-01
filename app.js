33b40558ca17c1245f61c24a8be2b597a9ba2b97
Julia Pabst <ju.pabst@gmail.com>
2024-03-01 19:50:41 +0100

    add timer

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+var timer = 0;
+var timeSpan = document.createElement("span");
+timeSpan.innerHTML = 1;
+let timeDiv = document.getElementById("time");
+timeDiv.appendChild(timeSpan);
+
 let player = document.createElement("span");
 player.innerHTML = prompt("What's your name?");
-
 let playerDiv = document.getElementById("player");
 playerDiv.appendChild(player);
 
+var intervalReference = setInterval(incrementTimer, 1000);
+
+function incrementTimer() {
+  timer++;
+  timeSpan.innerHTML = timer;
+}
+
+function stopp() {
+  clearInterval(intervalReference);
+}
